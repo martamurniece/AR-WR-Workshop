50c115fa48221a3a60b86aee54ce40fee0573f22
martamurniece <45332486+martamurniece@users.noreply.github.com>
2020-05-12 16:57:11 +0200

    animation: final changes

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,12 +1,26 @@
 
 
-// // changing individual properties with code and using setInterval
-// var rotationSpeed = 0.01;
-// var myOtherBox = document.getElementById('myOtherBox');
+// changing individual properties with code and using setInterval
 
-// function spin(){
-// 	myOtherBox.object3D.rotation.x += rotationSpeed;
-// 	console.log(myOtherBox.object3D.rotation.x);
-// }
+var rotationSpeed = 0.05;
+var positionSpeed = 0.05;
+var scaleSpeed = 0.01;
+var myOtherBox = document.getElementById('myOtherBox');
 
-// setInterval(spin, 16); //equivalent to 60 fps
+function spin() {
+	myOtherBox.object3D.rotation.x -= rotationSpeed;
+	myOtherBox.object3D.rotation.y -= rotationSpeed * 2;
+	myOtherBox.object3D.rotation.z -= rotationSpeed * 3;
+	myOtherBox.object3D.position.z -= positionSpeed;
+	myOtherBox.object3D.scale.y -= scaleSpeed * 3;
+	myOtherBox.object3D.scale.x -= scaleSpeed * 2;
+	myOtherBox.object3D.scale.z -= scaleSpeed * 2;
+}
+function flipSpeeds() {
+	rotationSpeed = -rotationSpeed;
+	positionSpeed = -positionSpeed;
+	scaleSpeed = -scaleSpeed;
+}
+
+setInterval(spin, 16); //equivalent to 60 fps
+setInterval(flipSpeeds, 5000);
